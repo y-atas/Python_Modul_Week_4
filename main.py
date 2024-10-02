fd26528a518934976211160884f75591aea406b3
Yakup Atas <yakup.atas.global@gmail.com>
2024-10-02 15:14:32 +0200

    start_1

---
+++ main.py
@@ -0,0 +1,10 @@
+import uye_islemleri
+import kitap_islemleri
+import json
+
+while (True) :
+    try:
+        print ("islemler")
+    except Exception as hata:
+        print ("Hata : ",hata,end = "\n\n")
+
